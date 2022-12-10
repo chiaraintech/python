ad19d727d2380df438042f459e4f94232a71d5c8
chiaraintech <chiaraintech@gmail.com>
2022-12-10 15:51:56 +0000

    static method annotation

--- encapsulation/main.py
+++ encapsulation/main.py
@@ -20,11 +20,23 @@ class Person:
             self.__name = "Default Name"
         else:
             self.__name = value
+    
+    # annotation not needed but best practice to hint
+    # not passing self bc a static method is not related to a specific object
+    # method that works without any object in the class directly
+    @staticmethod
+    def static_method():
+        print('hello world')
 
 
+# it works even if I don't have any object initialised at this point
+Person.static_method()
 
 p1 = Person("Chiara", 27, 'm')
 print(p1.Name)
+
+# same result as line 32 because the two things are not related
+p1.static_method()
 
 p1.Name = "Bob"
 print(p1.Name)
